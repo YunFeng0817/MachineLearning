18d198ec3bf2414f4bf9f6b57a1484dce2c9ef7f
Fitz <f18846188605@gmail.com>
2018-09-29 19:04:22 +0800

    添加了正则项

--- LAB1/main.py
+++ LAB1/main.py
@@ -29,20 +29,39 @@ def generateY(x, noise_mean, noise_variance):
     return mat(func(x) + rd.normal(noise_mean, noise_variance, x.shape[0]))
 
 
-def analytical(x, y):
+def analytical(x, y, Lambda):
     """
     get theta by analytical solution
     """
-    return linalg.inv(transpose(x)@x)@transpose(x)@y
-    return (x.T*x).I*x.T*y
+    if Lambda == '':
+        return (x.T*x).I*x.T*y
+    else:
+        return (x.T*x + Lambda).I*x.T*y
 
 
-def grandient_descent(x, y, min_step_length, max_iteration, gamma):
-    def gradient(theta, x, y):
+def grandient_descent(x, y, min_step_length, max_iteration, gamma, Lambda):
+    """
+    gradient descent solution
+    arguments:
+        x : origin data x
+        y : origin data y
+        min_step_length : descent min step
+        max_iteration :　max times to iterate
+        gamma : iteration step
+        lamda : regular term parameter
+    """
+    def gradient(theta, x, y, Lambda):
         """
         compute the gradient
+        arguments:
+            x : origin data x
+            y : origin data y
+            lamda : regular term parameter; if lamda is '',means without regular term
         """
-        return x.T * x * theta - x.T * y
+        if Ｌambda == '':
+            return x.T * x * theta - x.T * y
+        else:
+            return x.T * x * theta - x.T * y+Lambda*theta
 
     theta = mat(zeros(DEGREE+1)).reshape(DEGREE+1, 1)
     v = zeros(DEGREE+1).reshape(DEGREE+1, 1)
@@ -50,7 +69,7 @@ def grandient_descent(x, y, min_step_length, max_iteration, gamma):
     i = 0
     while ((step > min_step_length) and (i < max_iteration)):
         prev_position = theta
-        theta = theta - gamma * gradient(theta, x, y)
+        theta = theta - gamma * gradient(theta, x, y, Lambda)
         step = (theta - prev_position).T*(theta - prev_position)
         i = i + 1
     return theta
@@ -73,7 +92,7 @@ def plot(x1, y1, x2, theta):
     plt.show(fig1)
 
 
-DEGREE = 15
+DEGREE = 10
 START = -1  # origin data left interval
 END = 1  # origin data right interval
 ORIGIN_STEP = 0.04  # origin data step
@@ -83,13 +102,16 @@ MIN_STEP_LENGTH = 1e-9  # parameter for gradient descent
 NOISE_MEAN = 0
 NOISE_VARIANCE = 0.1
 GAMMA = 1e-2
+LAMBDA = 0.1  # regular term parameter
 x = arange(START, END, ORIGIN_STEP)
 X = generateX(x, DEGREE)
 Y = generateY(x, NOISE_MEAN, NOISE_VARIANCE).reshape(x.size, 1)
 
 
-theta = analytical(X, Y)
-theta = grandient_descent(X, Y, MIN_STEP_LENGTH, MAX_ITERATION, GAMMA)
+# theta = analytical(X, Y, '')
+# theta = analytical(X, Y, LAMBDA)
+theta = grandient_descent(X, Y, MIN_STEP_LENGTH, MAX_ITERATION, GAMMA, '')
+# theta = grandient_descent(X, Y, MIN_STEP_LENGTH, MAX_ITERATION, GAMMA, LAMBDA)
 
 
 plot(x, Y, arange(START, END, RESULT_STEP), theta)
