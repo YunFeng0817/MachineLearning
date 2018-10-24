f60e334403dcf8ab9f1f567494dfcc678fbe4df6
Fitz <f18846188605@gmail.com>
2018-10-24 20:10:37 +0800

    完善梯度下降法

--- LAB2/main.py
+++ LAB2/main.py
@@ -3,42 +3,60 @@ import numpy.random as rd
 import matplotlib.pyplot as plt
 import math
 
-DEGREE = 2
-DENSITY = 30
-X = []
-Y = []
-for i in range(1, DEGREE+1):
-    X.extend(rd.normal(0, 20, int(DENSITY/2)))
-    X.extend(rd.normal(50, 20, int(DENSITY/2)))
-    # X.extend(rd.normal(0, i, int(DENSITY/2)))
-    # X.extend(rd.normal(1, i, int(DENSITY/2)))
-Y.extend(zeros(int(DENSITY/2)))
-Y.extend(ones(int(DENSITY/2)))
-X = matrix(X).reshape(DEGREE, DENSITY).T
-Y = matrix(Y).reshape(DENSITY, 1)
+DEGREE = 2  # the degree of the X
+DENSITY = 50  # number of the generated data
+type1_mean = 0  # mean of type1's data
+type2_mean = 1  # mean of type2's data
+type1_variance = 0.5  # variance of type1's data
+type2_variance = 0.5  # variance of type2's data
+
+
+def generate_data():
+    '''
+    use gassus distribution to generate data
+    hypothesis:
+    each degree of X is independent
+    each degree of X has same variance
+    '''
+    X = []
+    Y = []
+    for i in range(1, DEGREE+1):
+        X.extend(rd.normal(type1_mean, type1_variance, int(DENSITY/2)))
+        X.extend(rd.normal(type2_mean, type2_variance, int(DENSITY/2)))
+    Y.extend(zeros(int(DENSITY/2)))
+    Y.extend(ones(int(DENSITY/2)))
+    X = mat(X).reshape(DEGREE, DENSITY).T
+    Y = mat(Y).reshape(DENSITY, 1)
+    return (X, Y)
 
 
 def plot(X, w):
-    fig1 = plt.figure('fig1')
-    plt.plot(X[:int(DENSITY/2):, 0], X[:int(DENSITY/2):, 1], 'ro')
-    plt.plot(X[int(DENSITY/2)::, 0], X[int(DENSITY/2)::, 1], 'go')
-    start = -50
-    end = 100
-    x = arange(start, end, (end-start) /
-               DENSITY).reshape(1, DENSITY)
-    y = (w[0]+w[1]*x)/(-w[2])
-    plt.plot(x, y, 'bo')
-    plt.show(fig1)
+    '''
+    draw the data set and the classification result
+    '''
+    plt.scatter(X[:int(DENSITY/2):, 0].tolist(),
+                X[:int(DENSITY/2):, 1].tolist(), label='class1')
+    plt.scatter(X[int(DENSITY/2)::, 0].tolist(),
+                X[int(DENSITY/2)::, 1].tolist(), label='class2')
+    start = type1_mean-2*type1_variance
+    end = type2_mean+2*type2_variance
+    x = linspace(start, end, 100)
+    w = w.tolist()
+    y = -(w[0]+w[1]*x)/w[2]
+    plt.plot(x, y)
+    plt.show()
 
 
 def Gradient(X, Y, w):
-    result = matrix(zeros(DEGREE+1)).reshape(DEGREE+1, 1)
+    '''
+    compute the gradient in current position according the w,X,Y
+    '''
+    result = mat(zeros(DEGREE+1)).reshape(DEGREE+1, 1)
     for i in range(DEGREE+1):
         sum = 0
         for j in range(DENSITY):
-            x = w[0,0]
-            temp = w[1::].T
-            x += (temp) * (X[j, :].T)
+            x = w[0, 0]
+            x += (w[1::].T) * (X[j, :].T)
             x = exp(x)
             x = 1 - 1/(1 + x)
             if i == 0:
@@ -49,17 +67,27 @@ def Gradient(X, Y, w):
     return result
 
 
-w = matrix(zeros(DEGREE+1)).reshape(DEGREE+1, 1)
-step = 1
-i = 0
-min_step_length = 1e-8
-max_iteration = 1000
-gamma = 1e-1
-while ((step > min_step_length) and (i < max_iteration)):
-    prev_position = w
-    temp = Gradient(X, Y, w)
-    w = w + gamma * temp
-    step = (w - prev_position).T*(w - prev_position)
-    i = i + 1
-print(w, i)
+def gradient_discent(X, Y):
+    '''
+    use gradient descent method to compute the logistic regression
+    '''
+    w = mat(zeros(DEGREE+1)).reshape(DEGREE+1, 1)
+    step = 1
+    i = 0
+    min_step_length = 1e-3
+    max_iteration = 5000
+    gamma = 1e-1
+    while ((step > min_step_length) and (i < max_iteration)):
+        prev_position = w
+        w = w + gamma * Gradient(X, Y, w)
+        step = (w - prev_position).T*(w - prev_position)
+        i = i + 1
+    print(w, i)
+    return w
+
+
+data = generate_data()
+X = data[0]
+Y = data[1]
+w = gradient_discent(X, Y)
 plot(X, w)
